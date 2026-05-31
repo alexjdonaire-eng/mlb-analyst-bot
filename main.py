96beb2d8f2e442b28daee721f7ea686539e28d1e
alexjdonaire-eng <alexjdonaire@gmail.com>
2026-05-31 17:38:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("MLB Analyst Bot iniciado correctamente")
+import os
+import requests
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+mensaje = """
+⚾ MLB Analyst Bot
+
+✅ Conexión exitosa
+
+El bot está funcionando correctamente en Railway.
+"""
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    json={
+        "chat_id": CHAT_ID,
+        "text": mensaje
+    }
+)
+
+print("Mensaje enviado")
