b8f70f6c9fa9efb47a6678df9e86da7b97ca1dc1
alexjdonaire-eng <alexjdonaire@gmail.com>
2026-06-01 07:21:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -85,19 +85,21 @@ def main():
 
     games = r.json()
 
-    for game in games:
+for game in games:
 
     home = game["home_team"]
     away = game["away_team"]
 
     book = game["bookmakers"][0]
     outcomes = book["markets"][0]["outcomes"]
+
     home_odds = None
     away_odds = None
 
     for o in outcomes:
         if o["name"] == home:
             home_odds = o["price"]
+
         if o["name"] == away:
             away_odds = o["price"]
 
