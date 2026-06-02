2e6839f743c418ad1fdd640842d86e614547da15
alexjdonaire-eng <alexjdonaire@gmail.com>
2026-06-02 15:09:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import analyzer
 
 print("🚀 BOT STARTED")
 
+print("1. Ejecutando collector...")
 collector.main()
+
+print("2. Ejecutando analyzer...")
 analyzer.main()
 
-print("DONE")
+print("3. DONE")
