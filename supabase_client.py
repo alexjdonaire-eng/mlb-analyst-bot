a02f250f7f9515998f5e646ed0c314dcc076f832
alexjdonaire-eng <alexjdonaire@gmail.com>
2026-06-05 00:06:19 -0400

    Log SUPABASE_URL and SUPABASE_KEY presence
    Added print statements to check environment variable values.

--- supabase_client.py
+++ supabase_client.py
@@ -4,6 +4,9 @@ import os
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
+print("SUPABASE_URL:", bool(SUPABASE_URL))
+print("SUPABASE_KEY:", bool(SUPABASE_KEY))
+
 supabase = create_client(
     SUPABASE_URL,
     SUPABASE_KEY
